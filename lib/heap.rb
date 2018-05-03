f76e4bcf8e6e1a913a70bc15190ac51e6ceed1c9
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-02 20:28:02 -0700

    Working heapify_down

--- lib/heap.rb
+++ lib/heap.rb
@@ -27,24 +27,34 @@ class BinaryMinHeap
 
   public
 
-  def is_valid?(parent_idx, child_idx, type)
-    parent = @store[parent_idx]
-    child = @store[child_idx]
-    
-    if type == 'min'
-      parent <= child
-    else
-      parent >= child
-    end
-  end
+  # def is_valid?(parent_idx, child_idx, type = 'min')
+  #   parent = @store[parent_idx]
+  #   child = @store[child_idx]
+  #
+  #   if type == 'min'
+  #     parent <= child
+  #   else
+  #     parent >= child
+  #   end
+  # end
+  #
+  # def swap(parent_idx)
+  #   children_indices = child_indices(count, parent_idx)
+  #
+  #   parent = @store[parent_idx]
+  #   child1 = @store[children_indices[0]] || nil
+  #   child2 = @store[children_indices[1]] || nil
+  #
+  #   if @prc.call(parent, child1) == -1
+  #     @store[parent_idx], @store[child_indices[0]] = @store[child_indices[0]], @store[parent_idx]
+  #   elsif @prc.call(parent, child2) == -1
+  #     @store[parent_idx], @store[child_indices[1]] = @store[child_indices[1]], @store[parent_idx]
+  #   end
+  # end
 
-  def swap(parent_idx, type)
-    if type == 'min'
-      children_indices = child_indices()
-    else
-
-    end
-  end
+  # def find_smaller()
+  #
+  # end
 
   def self.child_indices(len, parent_index)
     #Heaps 3 video 6:40
@@ -53,8 +63,8 @@ class BinaryMinHeap
     child1 = (2 * parent_index) + 1
     child2 = (2 * parent_index) + 2
 
-    children << child1 unless child1 >= len
-    children << child2 unless child2 >= len
+    children << child1 unless child1 >= len || child1.nil?
+    children << child2 unless child2 >= len || child2.nil?
 
     children
   end
@@ -65,8 +75,39 @@ class BinaryMinHeap
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    if prc.nil?
+      prc = Proc.new do |el1, el2|
+        (el1 <=> el2)
+      end
+    end
 
+    parent_idx.upto(len - 1) do |idx|
+      children_indices = child_indices(len, idx)
+
+      parent = array[idx]
+      child1 = nil
+      child2 = nil
+
+      if children_indices.empty?
+        next
+      elsif children_indices.length == 1
+        child1 = array[children_indices[0]]
+      elsif children_indices.length == 2
+        child1 = array[children_indices[0]]
+        child2 = array[children_indices[1]]
+      end
+
+      if (!child1.nil? && prc.call(parent, child1) == 1) && (child2.nil? || prc.call(child1, child2) != 1)
+        array[idx], array[children_indices[0]] = array[children_indices[0]], array[idx]
+      elsif !child2.nil? && prc.call(parent, child2) == 1
+        array[idx], array[children_indices[1]] = array[children_indices[1]], array[idx]
+      end
+    end
+
+    array
   end
+
+
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
   end
